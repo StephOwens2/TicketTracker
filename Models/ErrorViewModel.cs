b857e5edcc8602a0783f13b2e74a18f34aa90017
Stephanie Owens <StephOwens2@gmail.com>
2022-05-22 00:43:23 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNet_Core_6_TicketTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
